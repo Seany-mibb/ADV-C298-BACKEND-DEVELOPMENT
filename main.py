770b011a7f01f707eda250d8b8b89ee8e21077d4
Baevver <86206802+Seany-mibb@users.noreply.github.com>
2023-11-28 19:53:19 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-from paho.mqtt import client as mqtt_client
+import paho.mqtt.client as mqtt_client
 import time
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
-broker = 'broker.emqx.io'
-port = 1883
-topic = "topicName/iot"
-
-detection = 0
-
-client_id = 'test'
-username = 'emqx'
-password = ''
-
 def on_connect(client, userdata, flags, rc):
     client.subscribe("topicName/pir")
 
@@ -24,10 +13,10 @@ def on_message(client, userdata, msg):
 @app.route('/', methods=['GET'])
 
 def check_detection():
-    client = mqtt_client.Client(client_id)
+    client = mqtt_client.Client()
     client.on_connect = on_connect
     client.on_message = on_message
-    client.connect(broker, port)
+    client.connect("broker.emqx.io", 1883)
     client.loop_start()
 
     for i in range(0, 10):
